4b98dc4fc827623c2bc6c0d8fd9bc2befc1c10fa
Steve Conover <sconover@gmail.com>
2010-09-10 07:46:18 -0700

    slightly better test names

--- test_external/real_twitter_test.rb
+++ test_external/real_twitter_test.rb
@@ -24,27 +24,31 @@ regarding "really use twitter (this is a control, and to prove out a test suite,
   
   regarding "failure scenarios" do
 
-    test_GET "/1/statuses/user_timeline.ZZZZZZ?screen_name=gq_amy" do |body, headers, code|
+    test_GET "/1/statuses/user_timeline.ZZZZZZ?screen_name=gq_amy",
+             "bad document type" do |body, headers, code|
       assert{ code == 403 }
       assert{ headers["Content-Type"] == "text/html; charset=utf-8" }
       assert{ body.text.strip == "" }
     end
 
-    test_GET "/1/statuses/user_timeline.json?screen_name=gq_amyZZ" do |json, headers, code|
+    test_GET "/1/statuses/user_timeline.json?screen_name=gq_amyZZ", 
+             "bad screen name, json" do |json, headers, code|
       assert{ code == 404 }
       assert{ headers["Content-Type"] == "application/json; charset=utf-8" }
       assert{ json == {"request"=>"/1/statuses/user_timeline.json?screen_name=gq_amyZZ", 
                         "error"=>"Not found"} }
     end
 
-    test_GET "/1/statuses/user_timeline.xml?screen_name=gq_amyZZ" do |xml, headers, code|
+    test_GET "/1/statuses/user_timeline.xml?screen_name=gq_amyZZ", 
+             "bad screen name, xml" do |xml, headers, code|
       assert{ code == 404 }
       assert{ headers["Content-Type"] == "application/xml; charset=utf-8" }
       assert{ xml.xpath("/hash/request").first.text == "/1/statuses/user_timeline.xml?screen_name=gq_amyZZ" }
       assert{ xml.xpath("/hash/error").first.text == "Not found" }
     end
     
-    test_GET "/4/statuses/user_timeline.json?screen_name=gq_amy", "invalid version" do |html, headers, code|
+    test_GET "/4/statuses/user_timeline.json?screen_name=gq_amy", 
+             "invalid version" do |html, headers, code|
       assert{ code == 404 }
       assert{ headers["Content-Type"] == "text/html; charset=utf-8" }
       assert{ html.to_s.include?("Sorry, that page doesn’t exist!") }
